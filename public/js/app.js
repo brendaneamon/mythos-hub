9a06aeaae8a3bca6b2590168c1dd554e4732eeee
Brendan Magee <bmagee@gmail.com>
2016-04-26 10:08:21 -0400

    added factory; index view working; added stylesheet

--- public/js/app.js
+++ public/js/app.js
@@ -10,11 +10,40 @@
     "$stateProvider",
     Router
   ])
-  .controller("mythRefsIndexCtrl", [
-    "Show",
+  .factory("MythReference", [
+    "$resource",
+    MythReference
+  ])
+  .controller("MythRefsIndexCtrl", [
+    "MythReference",
     "$state",
-    showIndexCtrl
+    MythRefsIndexCtrl
   ]);
+
+  // Myth Reference factory function is here
+
+  function MythReference ($resource) {
+    var MythRef = $resource("/api/myth-references/:title", {}, {
+      update: {method: "PUT"}
+    });
+    MythRef.all = MythRef.query();
+    MythRef.find = function(property, value, callback){
+      MythRef.all.$promise.then(function(){
+        MythRef.all.forEach(function(reference){
+          if(reference[property] == value) callback(reference);
+        });
+      });
+    };
+    return MythRef;
+  }
+
+// Myth Reference index controller function
+
+function MythRefsIndexCtrl (MythReference, $state) {
+  this.mythRefs = MythReference.all;
+}
+
+// Router function
 
   function Router ($stateProvider) {
     $stateProvider
@@ -25,7 +54,7 @@
     .state("index", {
       url: "/myth-references",
       templateUrl: "/assets/html/myth-references-index.html",
-      controller: "mythRefsIndexCtrl",
+      controller: "MythRefsIndexCtrl",
       controllerAs: "indexVM"
     })
     .state("show", {
