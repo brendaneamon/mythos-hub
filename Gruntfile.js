d7d115a9ff486eaede4f61a1d776c90c16d5478e
Brendan Magee <bmagee@gmail.com>
2016-10-10 15:02:15 -0400

    feature(Gruntfile): Use `load-grunt-tasks`

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,7 @@
 module.exports = function (grunt) {
+  
+  require('load-grunt-tasks')(grunt);
+  
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     clean: ['build/*'],
@@ -43,13 +46,6 @@ module.exports = function (grunt) {
       tasks: ['jshint']
     }
   });
-  
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-clean');
   
   grunt.registerTask('default', ['jshint', 'clean', 'concat', 'uglify']);
 };
